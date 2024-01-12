c518590f503f884d04bb581700b4e78e67ffdc2c
elenaermolaeva <elena161284@mail.ru>
2024-01-12 23:14:36 +0300

    start

---
+++ src/main/java/com/example/lkm_genc_bot/LkmGencBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.lkm_genc_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkmGencBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LkmGencBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lkm_genc_bot/LkmGencBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lkm_genc_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LkmGencBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
